3850eadb04448c35db79a3d7531d7adc4a04ad9e
prabhakar.pandey <vky535kolkata@gmail.com>
2021-03-04 16:42:29 +0530

    call me once interview question

---
+++ intervews/callMethodOnce.js
@@ -0,0 +1,21 @@
+function greetings(name){
+    return `hello ${name}!`;
+}
+
+
+function callMeOnce(fn){
+    var done = false;
+    return function(name){
+        if(!done){
+            done = true;
+            return fn.call(null,name);       
+        }else{
+            return undefined;
+        }
+        
+    }
+}
+let callMeOnceWrapper = new callMeOnce(greetings);
+
+console.log(callMeOnceWrapper("Prabhakar"));
+console.log(callMeOnceWrapper("Prabhakar"));
